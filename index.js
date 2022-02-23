09e1d6d2a21579d173c1b1e9704021abf3dc84ec
enoch harnanto <enochharnantoo@gmail.com>
2022-02-23 13:04:55 -0800

    Added functionality to create new book object and append it to library

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const libraryContainer = document.getElementById('library-container');
 const addBookBtn = document.getElementById('add-btn');
 const modal = document.getElementById('add-modal');
 const span = document.getElementsByClassName("close")[0];
-let myLibrary = [];
+const submit = document.getElementById('submit');
 
-
-const myBook = new Book('Gospel According to Jesus', 'John Macarthur', '500');
-addBookBtn.addEventListener("click", checkLib);
+addBookBtn.addEventListener("click", openModal);
 span.addEventListener("click", closeModal);
+submit.addEventListener("click", getNewBook);
 
-function checkLib() {
+let myLibrary = [];
+const myBook = new Book('Gospel According to Jesus', 'John Macarthur', '500');
+addBookToLibrary(myBook);
+console.log(Object.values(myBook));
+console.log(myLibrary);
+/* Open/Close Modal */
+function openModal() {
     modal.style.display = "block";
-    // addBook(myBook);
-    // console.log(myLibrary);
-    // displayBook(); 
-    // console.log(libraryContainer.children);
 }
 
 function closeModal() {
@@ -27,15 +28,48 @@ window.onclick = function(e) {
     }
 }
 
+function isInLibrary(book) {
+    let title = myLibrary.find((oldBook, index) => {
+        if(book.title === oldBook.title) {
+            return true;
+        }
+        return false;
+    })
+}
+
+/* Adding New Book */
+function addBookFromInput() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    return new Book(title, author, pages);
+}
+
+function getNewBook(e) {
+    e.preventDefault();
+    const newBook = addBookFromInput();
+    addBookToLibrary(newBook);
+    console.log(Object.values(newBook));
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.log("book added to library!");
+    console.log(myLibrary);
+}
+
+function iterateLib() {
+    for(let books in myLibrary) {
+        console.log(books.title);
+    }
+}
+
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addBook(book) {
-    myLibrary.push(book);
-}
 
 function displayBook() {
     for (let book of myLibrary) {
